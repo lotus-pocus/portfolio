300d0d5c7513c40c11227c7a1b1f4eeebfa3bba5
lotus-pocus <116104241+lotus-pocus@users.noreply.github.com>
2025-07-30 20:20:51 +0100

    updated projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,35 +1,42 @@
-// Import data from data.json file
-import project from './project.json';
-
-
+// src/components/Project.js
+import project from "./project.json";
 
 function Project() {
-
   return (
-    <main className='column'>
-
-      <h1>Projects: </h1>
-      <br></br>
+    <main className="column">
+      <h1>Projects:</h1>
+      <br />
       <p>Scroll down to have a look at some of my projects.</p>
-      <br></br>
+      <br />
 
-      {/* Use the map() method to output a <div> for each project */}
+      {project.map((Object, index) => (
+        <div className="container" key={index}>
+          <h2 style={{ color: "grey" }}>{Object.name}</h2>
+          <br />
+          {/* Direct path to image in /public/images/ */}
+          <img
+            className="projectImg"
+            style={{ width: "800px", height: "600px" }}
+            src={`${process.env.PUBLIC_URL}${Object.image}`}
+            alt={`Project: ${Object.name}`}
+          />
 
-      {project.map((Object, index) => {
-        return (
-          <div className="container" key={index}>
-            <h2 style={{ color: 'grey' }}>{Object.name}</h2>
-            <br></br>
-            <img className="projectImg" style={{ width: '800px', height: '600px' }} src={require(`../image/image0${index + 1}.png`)} alt="project image" />
-            <br></br>
-            <a href={Object.url} target="_blank">project</a>
-          </div>
-        )
-      })}
-
+          <br />
+          <a href={Object.url} target="_blank" rel="noopener noreferrer">
+            View project
+          </a>
+          <br />
+          {/* Optional: Show description if present */}
+          {Object.description && (
+            <>
+              <br />
+              <p>{Object.description}</p>
+            </>
+          )}
+        </div>
+      ))}
     </main>
-
-  )
+  );
 }
 
 export default Project;
